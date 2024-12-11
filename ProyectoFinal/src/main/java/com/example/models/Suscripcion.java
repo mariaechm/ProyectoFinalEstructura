154daecdb842ba5162a8c70760af0bf45838a974
Maria Elizabeth Chuico Medina <166523237+mariaechm@users.noreply.github.com>
2024-12-11 10:53:18 -0500

    Backend

--- ProyectoFinal/src/main/java/com/example/models/Suscripcion.java
+++ ProyectoFinal/src/main/java/com/example/models/Suscripcion.java
@@ -1,5 +1,43 @@
 package com.example.models;
+import com.example.models.enumerator.TipoSuscripcion;
 
 public class Suscripcion {
+
+    private Integer id;
+    private String fechaInicio;
+    private String fechaFinalizacion;
+    private TipoSuscripcion tipo;
+    
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFechaInicio() {
+        return this.fechaInicio;
+    }
+
+    public void setFechaInicio(String fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public String getFechaFinalizacion() {
+        return this.fechaFinalizacion;
+    }
+
+    public void setFechaFinalizacion(String fechaFinalizacion) {
+        this.fechaFinalizacion = fechaFinalizacion;
+    }
+
+    public TipoSuscripcion getTipo() {
+        return this.tipo;
+    }
+
+    public void setTipo(TipoSuscripcion tipo) {
+        this.tipo = tipo;
+    }
     
 }
