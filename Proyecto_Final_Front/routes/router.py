5f1a7e9e7d2149f9e03758158b550f39d1b61e89
Maria Elizabeth Chuico Medina <166523237+mariaechm@users.noreply.github.com>
2024-12-12 12:47:21 -0500

    version 1.1
    correccion de errores

--- Proyecto_Final_Front/routes/router.py
+++ Proyecto_Final_Front/routes/router.py
@@ -1,10 +1,7 @@
 from flask import render_template, redirect, request, flash, Blueprint
 import requests
 import json
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/moduloEstadistica
 router = Blueprint('router',__name__)
 
 @router.route('/admin')
